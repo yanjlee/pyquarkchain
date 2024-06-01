cc4339431b15cfa18a7f37647a76a5aee35a2b31
shadow <yanjlee@163.com>
2024-06-01 18:56:58 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,49 +1,63 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Name    : setup.py
+# @Author  : yanlee
 
-from setuptools import setup
-from setuptools.command.develop import develop
+import setuptools
+import shutil
 
-install_requires = set(x.strip() for x in open("requirements.txt"))
-install_requires_replacements = {}
-install_requires = [install_requires_replacements.get(r, r) for r in install_requires]
-PTH = "pypy-fix-cython-warning.pth"
+# 删除dist/目录
+shutil.rmtree('dist', ignore_errors=True)
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-
-
-class custom_develop(develop):
-    @property
-    def target(self):
-        return os.path.join(self.install_dir, PTH)
-
-    def run(self):
-        print(self.install_dir)
-        with open("quarkchain/tools/" + PTH) as infp:
-            with open(self.target, "w") as outfp:
-                outfp.write(infp.read())
-        develop.run(self)
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    name="quarkchain",
-    version="0.0",
-    author="QuarkChain",
-    author_email="",
-    description=("QuarkChain"),
-    license="MIT",
-    keywords="QuarkChain,blockchain",
-    url="",
-    packages=["quarkchain"],
-    long_description=read("README.md"),
-    classifiers=["Development Status :: 0 - Development", "License :: MIT License"],
-    install_requires=install_requires,
-    python_requires=">=3.5",
-    cmdclass={"develop": custom_develop},
+setuptools.setup(
+    name="pyquarkchain-pro",
+    version="2.2.3",
+    author="yanjlee",
+    author_email="yanjlee@163.com",
+    description="QuarkChain is a sharded blockchain protocol that employs a two-layer architecture - one extensible sharding layer consisting of multiple shard chains processing transactions and one root chain layer securing the network and coordinating cross-shard transactions among shard chains. The capacity of the network scales linearly as the number of shard chains increase while the root chain is always providing strong security guarantee regardless of the number of shards. QuarkChain testnet consistently hit [10,000+ TPS](https://youtu.be/dUldrq3zKwE?t=8m28s) with 256 shards run by 50 clusters consisting of 6450 servers with each loadtest submitting 3,000,000 transactions to the network.",  # 模块简介
+    install_requires=[
+        'requests',
+        'faker',
+        'execjs',
+        'loguru',
+        'base64',
+        'hashlib',
+        'Crypto',
+        'pandas',
+        'fuzzywuzzy',
+        'httpx',
+        'Pillow',
+        'playwright',
+        'PyExecJS',
+        'redis',
+        'fastapi',
+        'uvicorn',
+        'APScheduler',
+        'beautifulsoup4',
+        'bs4',
+        'certifi',
+        'clickhouse-driver',
+        'curl-cffi',
+        'DrissionPage',
+        'fake-useragent',
+        'Flask',
+        'Flask-APScheduler',
+        'Flask-Cors',
+        'frida',
+        'gevent',
+        'httpx',
+        'Jinja2',
+        'langchain',
+        'langchain-community',
+        'suiutils-py',
+    ],
+    long_description=open(r'readme.md', encoding='utf-8').read(),  # 读取readme自述文件
+    long_description_content_type="text/markdown",
+    url="https://github.com/yanjlee/pyquarkchain",  # 模块github地址
+    packages=setuptools.find_packages(),     # 自动列出项目下的包
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",   # 开源许可证
+        "Operating System :: OS Independent",      # 这里的定义是系统无关（全平台兼容），如果你的包只能在部分特定系统上运行，需要修改。
+    ],
 )
